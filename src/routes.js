a79fb6f5c41618fc81805f95db52c63223b72815
Malindu Madhusankha <dpmmadhusankha@gmail.com>
2022-05-18 00:34:12 +0530

    test3

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 //layouts
 import MainLayout from "./layouts/MainLayout";
 import ClerkLayout from "./layouts/ClerkLayout";
+import ApplicantLayout from "./layouts/ApplicantLayout";
+import SecretaryLayout from "./layouts/SecretaryLayout";
+import ReviewerLayout from "./layouts/ReviewerLayout";
 
 //componets
 import SignIn from "./components/SignIn";
@@ -17,6 +20,30 @@ const routes = (isAuthenticated, userRole) => [
    {
       path: "/clerk",
       element: <ClerkLayout />,
+      children: [
+         { path: "dashboard", element: <h1>Hello</h1> },
+         { path: "*", element: <h1>Not Found 404</h1> },
+      ],
+   },
+   {
+      path: "/applicant",
+      element: <ApplicantLayout />,
+      children: [
+         { path: "dashboard", element: <h1>Hello</h1> },
+         { path: "*", element: <h1>Not Found 404</h1> },
+      ],
+   },
+   {
+      path: "/secretary",
+      element: <SecretaryLayout />,
+      children: [
+         { path: "dashboard", element: <h1>Hello</h1> },
+         { path: "*", element: <h1>Not Found 404</h1> },
+      ],
+   },
+   {
+      path: "/reviewer",
+      element: <ReviewerLayout />,
       children: [
          { path: "dashboard", element: <h1>Hello</h1> },
          { path: "*", element: <h1>Not Found 404</h1> },
