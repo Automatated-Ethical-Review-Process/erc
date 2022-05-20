1bd9b36d2489363e3666cd4b12a112366fe7f8bc
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-05-20 11:56:11 +0530

    did changes to background images

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -115,46 +115,50 @@ export default function SignIn() {
             container
             spacing={2}
             columns={{ xs: 4, md: 12 }}
-            sx={{ mt: 0.5 }}
+            sx={{ mt: 0.5, mx: 0.5 }}
          >
-            <Grid item xs={4} md={7}>
-               <Item
+            <Grid item xs={4} md={7} sx={{justifyContent:'center',alignItems:'center'}}>
+               <Container
+                  maxWidth="md"
                   sx={{
                      backgroundImage: `url(${Image})`,
-                     height: "100%",
+                     height: "75vh",
                      width: "100%",
-                     opacity: 1,
+                     opacity: 0.7,
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center",
                      backgroundSize: "cover",
+                     position:'absolute',
+                     zIndex:-1
+                  }}
+               ></Container>
+               <Typography
+                  variant="h6"
+                  color="initial"
+                  align="justify"
+                  paragraph
+                  sx={{
+                     fontWeight: 700,
+                     fontFamily: "monospace",
+                     letterSpacing: "o.1rem",
+                     p: 2,
+                     zIndex:2001
                   }}
                >
-                  <Typography>
-                     Ethical review Committee (ERC), Faculty of Medicine (FOM),
-                     University of Ruhuna (UOR) reviews all types of research
-                     proposals involving human and animal studies. It was
-                     established in 1980. Membership of Federation of Ethical
-                     Review Committees of Sri Lanka (FERCSL) was obtained by
-                     ERC, FOM, UOR in 2017. The objective is to maintain
-                     standards of practice in research, including protection of
-                     human participants, animals and other living organisms,
-                     while promoting high quality research which is ethically
-                     and scientifically sound. The ERC is involved in the
-                     capacity building in research ethics and clinical ethics
-                     among the academics, students and the scientific community
-                     in Southern Province.Ethics review committee, Faculty of
-                     Medicine, University of Ruhuna underwent the international
-                     recognition survey conducted by the Forum for Ethical
-                     Review Committees in the Asian and Western Pacific (FERCAP)
-                     region during 28th to 30th June 2018. It was an extensive
-                     survey by a team of four surveyors (including two foreign
-                     surveyors) and nine survey trainees. The committee awarded
-                     the SIDCER (Strategic Initiative for Developing Capacity in
-                     Ethical Review) recognition at the FERCAP conference, Chang
-                     Gung Memorial Hospital,Taoyuan City, Taiwan 18th -21st
-                     November 2018.
-                  </Typography>
-               </Item>
+                  Ethical review Committee (ERC), Faculty of Medicine (FOM),
+                  University of Ruhuna (UOR) reviews all types of research
+                  proposals involving human and animal studies. It was
+                  established in 1980. Membership of Federation of Ethical
+                  Review Committees of Sri Lanka (FERCSL) was obtained by ERC,
+                  FOM, UOR in 2017. The objective is to maintain standards of
+                  practice in research, including protection of human
+                  participants, animals and other living organisms, while
+                  promoting high quality research which is ethically and
+                  scientifically sound. The ERC is involved in the capacity
+                  building in research ethics and clinical ethics among the
+                  academics, students and the scientific community in Southern
+                  Province.
+               </Typography>
             </Grid>
             <Grid item xs={4}>
                {formView}
