d858cf67503413ad96c11006383cac8451f61c03
rking32 <sljohnwick@gmail.com>
2022-05-24 13:20:15 +0530

    fixed some warnings

--- src/containers/Dashboard/Applicant/NewSubmission/NewSubmission.js
+++ src/containers/Dashboard/Applicant/NewSubmission/NewSubmission.js
@@ -1,13 +1,10 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Container, Grid } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
-
-
-
 
 const steps = ["Step 1", "Step 2"];
 
@@ -22,94 +19,95 @@ function getStepContent(step) {
    }
 }
 
-
 export default function HorizontalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+   const [activeStep, setActiveStep] = React.useState(0);
+   const [skipped, setSkipped] = React.useState(new Set());
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
+   const isStepOptional = (step) => {
+      return step === 1;
+   };
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
+   const isStepSkipped = (step) => {
+      return skipped.has(step);
+   };
 
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
+   const handleNext = () => {
+      let newSkipped = skipped;
+      if (isStepSkipped(activeStep)) {
+         newSkipped = new Set(newSkipped.values());
+         newSkipped.delete(activeStep);
+      }
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      setSkipped(newSkipped);
+   };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+   const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+   };
 
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
+   const handleSkip = () => {
+      if (!isStepOptional(activeStep)) {
+         // You probably want to guard against something like this,
+         // it should never occur unless someone's actively trying to break something.
+         throw new Error("You can't skip a step that isn't optional.");
+      }
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      setSkipped((prevSkipped) => {
+         const newSkipped = new Set(prevSkipped.values());
+         newSkipped.add(activeStep);
+         return newSkipped;
+      });
+   };
 
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+   const handleReset = () => {
+      setActiveStep(0);
+   };
 
-  return (
+   return (
       <Container>
-        <Box sx={{ width: '100%',minHeight:300}}>
-        {activeStep === steps.length ? (
-            <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-                All steps completed - you&apos;re finished
-            </Typography>
-            
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                <Box sx={{ flex: '1 1 auto' }} />
-                
-                <Button onClick={handleReset} variant='outlined'>Ok</Button>
-                
-            </Box>
-            </React.Fragment>
-        ) : (
-            <React.Fragment>
-                {getStepContent(activeStep)}
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2, }}>
-                <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-                variant='outlined'
-                >
-                Back
-                </Button>
-                <Box sx={{ flex: '1 1 auto' }} />
+         <Box sx={{ width: "100%", minHeight: 300 }}>
+            {activeStep === steps.length ? (
+               <React.Fragment>
+                  <Typography sx={{ mt: 2, mb: 1 }}>
+                     All steps completed - you&apos;re finished
+                  </Typography>
 
-                
-                <Button onClick={handleNext} variant='outlined' sx={{bottom:0}}>
-                {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
-                </Button>
-                
-            </Box>
-            </React.Fragment>
+                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                     <Box sx={{ flex: "1 1 auto" }} />
+
+                     <Button onClick={handleReset} variant="outlined">
+                        Ok
+                     </Button>
+                  </Box>
+               </React.Fragment>
+            ) : (
+               <React.Fragment>
+                  {getStepContent(activeStep)}
+                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                     <Button
+                        color="inherit"
+                        disabled={activeStep === 0}
+                        onClick={handleBack}
+                        sx={{ mr: 1 }}
+                        variant="outlined"
+                     >
+                        Back
+                     </Button>
+                     <Box sx={{ flex: "1 1 auto" }} />
+
+                     <Button
+                        onClick={handleNext}
+                        variant="outlined"
+                        sx={{ bottom: 0 }}
+                     >
+                        {activeStep === steps.length - 1 ? "Submit" : "Next"}
+                     </Button>
+                  </Box>
+               </React.Fragment>
             )}
-        
-        </Box>
-    </Container>
-  );
+         </Box>
+      </Container>
+   );
 }
--- src/containers/Dashboard/Applicant/NewSubmission/Step1.js
+++ src/containers/Dashboard/Applicant/NewSubmission/Step1.js
@@ -1,141 +1,156 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
-import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import { useState } from 'react';
-
-
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import { useState } from "react";
 
 export function FormControlLabelPosition(props) {
-  return (
-    <FormControl component="fieldset">
-      <FormGroup aria-label="position" row>
-        <FormControlLabel
-          value="start"
-          control={<Switch color="primary" onChange={()=>props.setSwitch(!props.defaultValue)}/>}
-          label={props.lbl}
-          labelPlacement="start"
-        />
-      </FormGroup>
-    </FormControl>
-  );
+   return (
+      <FormControl component="fieldset">
+         <FormGroup aria-label="position" row>
+            <FormControlLabel
+               value="start"
+               control={
+                  <Switch
+                     color="primary"
+                     onChange={() => props.setSwitch(!props.defaultValue)}
+                  />
+               }
+               label={props.lbl}
+               labelPlacement="start"
+            />
+         </FormGroup>
+      </FormControl>
+   );
 }
 
-const Input = styled('input')({
-  display: 'none',
+const Input = styled("input")({
+   display: "none",
 });
 
 export function UploadButton(props) {
-  return (
-    <Stack direction="row" alignItems="center">
-      <label htmlFor="contained-button-file">
-        <Input accept="image/*" id="contained-button-file" multiple type="file" />
-        <Button variant="outlined" component="span" >
-          {props.upload}
-        </Button>
-      </label>
-      <label htmlFor="icon-button-file">
-        <Input accept="image/*" id="icon-button-file" type="file" />
-        <IconButton color="primary" aria-label="upload picture" component="span" >
-          <DriveFolderUploadIcon />
-        </IconButton>
-      </label>
-    </Stack>
-  );
+   return (
+      <Stack direction="row" alignItems="center">
+         <label htmlFor="contained-button-file">
+            <Input
+               accept="image/*"
+               id="contained-button-file"
+               multiple
+               type="file"
+            />
+            <Button variant="outlined" component="span">
+               {props.upload}
+            </Button>
+         </label>
+         <label htmlFor="icon-button-file">
+            <Input accept="image/*" id="icon-button-file" type="file" />
+            <IconButton
+               color="primary"
+               aria-label="upload picture"
+               component="span"
+            >
+               <DriveFolderUploadIcon />
+            </IconButton>
+         </label>
+      </Stack>
+   );
 }
 
 export function BasicTextFields(props) {
-  return (
-    <Box
-
-      component="form"
-      sx={{
-        '& > :not(style)': { },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField 
-        sx={{width: {xs:'20vw',md:'60vw'} }}
-        id="outlined-basic" 
-        label={props.label} 
-        variant="outlined" 
-        size='small'
-      />
-    </Box>
-  );
+   return (
+      <Box
+         component="form"
+         sx={{
+            "& > :not(style)": {},
+         }}
+         noValidate
+         autoComplete="off"
+      >
+         <TextField
+            sx={{ width: { xs: "20vw", md: "60vw" } }}
+            id="outlined-basic"
+            label={props.label}
+            variant="outlined"
+            size="small"
+         />
+      </Box>
+   );
 }
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
 export default function FullWidthGrid() {
-  const [hasCoverLetter , setHasCoverLetter] = useState(false);
-  const [hasCV , setHasCV] = useState(false);
-  const [hasTrainingCertificate , setHasTrainingCertificate] = useState(false);
-  return (
+   const [hasCoverLetter, setHasCoverLetter] = useState(false);
+   const [hasCV, setHasCV] = useState(false);
+   const [hasTrainingCertificate, setHasTrainingCertificate] = useState(false);
+   return (
       <Container>
-        <Box sx={{ flexGrow: 1,mx:10,my:5 }} >
-        <Grid container spacing={1} >
-            <Grid item xs={12} md={12}>
-                <BasicTextFields label="Proposal Name" />
+         <Box sx={{ flexGrow: 1, mx: 10, my: 5 }}>
+            <Grid container spacing={1}>
+               <Grid item xs={12} md={12}>
+                  <BasicTextFields label="Proposal Name" />
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <BasicTextFields label="Proposal Type" />
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <BasicTextFields label="Co-Investigators" />
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <UploadButton upload="Upload the application form" />
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <UploadButton upload="Upload the proposal" />
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <Stack direction="row" spacing={2}>
+                     <FormControlLabelPosition
+                        lbl="Has cover letter?"
+                        setSwitch={setHasCoverLetter}
+                        defaultValue={hasCoverLetter}
+                     />
+                     {hasCoverLetter && <UploadButton upload="Upload" />}
+                  </Stack>
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <Stack direction="row" spacing={2}>
+                     <FormControlLabelPosition
+                        lbl="Has CV?"
+                        setSwitch={setHasCV}
+                        defaultValue={hasCV}
+                     />
+                     {hasCV && <UploadButton upload="Upload" />}
+                  </Stack>
+               </Grid>
+               <Grid item xs={12} md={12}>
+                  <Stack direction="row" spacing={2}>
+                     <FormControlLabelPosition
+                        lbl="Has training certificate?"
+                        setSwitch={setHasTrainingCertificate}
+                        defaultValue={hasTrainingCertificate}
+                     />
+                     {hasTrainingCertificate && (
+                        <UploadButton upload="Upload" />
+                     )}
+                  </Stack>
+               </Grid>
+               <Grid item xs={3} md={4}>
+                  <Typography varient={"h6"}>
+                     Upload the supplementary
+                  </Typography>
+               </Grid>
+               <Grid item xs={6} md={8}></Grid>
             </Grid>
-            <Grid item xs={12} md={12}>
-                <BasicTextFields label="Proposal Type" />
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <BasicTextFields label="Co-Investigators" />
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <UploadButton upload="Upload the application form"/>
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <UploadButton upload="Upload the proposal"/>
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <Stack direction="row" spacing={2}>
-                    <FormControlLabelPosition lbl="Has cover letter?" setSwitch={setHasCoverLetter} defaultValue={hasCoverLetter} />
-                    {hasCoverLetter && <UploadButton upload="Upload"/>}
-                </Stack>
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <Stack direction="row" spacing={2}>
-                    <FormControlLabelPosition lbl="Has CV?" setSwitch={setHasCV} defaultValue={hasCV}/>
-                    {hasCV && <UploadButton upload="Upload"/>}
-                </Stack>
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <Stack direction="row" spacing={2}>
-                    <FormControlLabelPosition lbl="Has training certificate?" setSwitch={setHasTrainingCertificate} defaultValue={hasTrainingCertificate}/>
-                    {hasTrainingCertificate && <UploadButton upload="Upload"/>}
-                </Stack>
-            </Grid>
-            <Grid item xs={3} md={4}>
-                <Typography varient={"h6"}>Upload the supplementary</Typography>
-            </Grid>
-            <Grid item xs={6} md={8}>
-            
-            </Grid>
-        </Grid>
-        </Box>
-    </Container>    
-  );
+         </Box>
+      </Container>
+   );
 }
