b05bc7a44770886b100f0850d005b6a4c9c2d378
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-05-24 13:07:17 +0530

    did changes to home  page view

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -102,21 +102,57 @@ export default function SignIn() {
       </Container>
    );
 
-   const Item = styled(Paper)(({ theme }) => ({
-      backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-      ...theme.typography.body2,
-      padding: theme.spacing(1),
-      textAlign: "center",
-      color: theme.palette.text.secondary,
-   }));
    return (
       <React.Fragment>
          <Grid
             container
-            spacing={2}
+            spacing={0}
             columns={{ xs: 4, md: 12 }}
-            sx={{ mt: 0.5, mx: 0.5 }}
+            sx={{ mt: 1, mx: 1 }}
          >
+            <Grid item xs={4} md={7}>
+               <Container
+                  sx={{
+                     backgroundImage: `url(${Image})`,
+                     height: "100%",
+                     width: "100%",
+                     opacity: 1,
+                     backgroundRepeat: "no-repeat",
+                     backgroundPosition: "center",
+                     backgroundSize: "cover",
+                     
+                  }}
+               >
+                  <Typography
+                     align="justify"
+                     sx={{ fontFamily: "monospace", letterSpacing: 2 }}
+                  >
+                     Ethical review Committee (ERC), Faculty of Medicine (FOM),
+                     University of Ruhuna (UOR) reviews all types of research
+                     proposals involving human and animal studies. It was
+                     established in 1980. Membership of Federation of Ethical
+                     Review Committees of Sri Lanka (FERCSL) was obtained by
+                     ERC, FOM, UOR in 2017. The objective is to maintain
+                     standards of practice in research, including protection of
+                     human participants, animals and other living organisms,
+                     while promoting high quality research which is ethically
+                     and scientifically sound. The ERC is involved in the
+                     capacity building in research ethics and clinical ethics
+                     among the academics, students and the scientific community
+                     in Southern Province.Ethics review committee, Faculty of
+                     Medicine, University of Ruhuna underwent the international
+                     recognition survey conducted by the Forum for Ethical
+                     Review Committees in the Asian and Western Pacific (FERCAP)
+                     region during 28th to 30th June 2018. It was an extensive
+                     survey by a team of four surveyors (including two foreign
+                     surveyors) and nine survey trainees. The committee awarded
+                     the SIDCER (Strategic Initiative for Developing Capacity in
+                     Ethical Review) recognition at the FERCAP conference, Chang
+                     Gung Memorial Hospital,Taoyuan City, Taiwan 18th -21st
+                     November 2018.
+                  </Typography>
+               </Container>
+            </Grid>
             <Grid item xs={4}>
                {formView}
             </Grid>
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -4,6 +4,7 @@ import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 
 import { Outlet } from "react-router-dom";
+
 export default function MainLayout() {
    return (
       <div>
