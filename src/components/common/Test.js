a3df74a0945a437bec088fb27d3a2c04fd3fd091
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-05-25 00:45:08 +0530

    Intergrate with pdf viewer

--- src/components/common/Test.js
+++ src/components/common/Test.js
@@ -1,12 +1,17 @@
 import AButton from "./AButton";
 import { useParams } from "react-router-dom";
+import PdfViewer from "../PdfViewer";
+import { ScopedCssBaseline } from "@mui/material";
 
 const Test = () => {
    let { id } = useParams();
    return (
       <div>
-         <AButton variant="contained" onClick={() => console.log("first")}>Click Me</AButton>
-         <h1>{id}</h1>
+         {/* {<AButton variant="contained" onClick={() => console.log("first")}>Click Me</AButton>
+         <h1>{id}</h1>} */}
+         <ScopedCssBaseline>
+            <PdfViewer />
+         </ScopedCssBaseline>
       </div>
    );
 };
