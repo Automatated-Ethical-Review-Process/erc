201f39ff6913663fe3182bb0648910f938129847
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-05-24 23:16:06 +0530

    implemnted customized button component

---
+++ src/components/common/AButton.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Button } from "@mui/material";
+
+import { ThemeContext } from "../../context/ThemeContext";
+const AButton = (props) => {
+   const { theme } = useContext(ThemeContext);
+   return (
+      <Button
+         {...props}
+         sx={{
+            backgroundColor: theme.color.main.primary,
+            borderColor: theme.color.main.primary,
+            fontFamily:theme.font.button.family,
+            fontWeight:700,
+            "&:hover": {
+               boxShadow: 10,
+               color: "white",
+               backgroundColor: theme.color.main.primary,
+               borderColor: theme.color.main.primary,
+               transitionDuration: "0.4s",
+            },
+         }}
+      >
+         {props.children}
+      </Button>
+   );
+};
+
+export default AButton;
