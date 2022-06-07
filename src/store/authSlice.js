309f2adc0897b533d096de7716f372d2a7337cbb
dilshan <nishadadilshan@gmail.com>
2022-06-07 11:54:43 +0530

    changed auth slice for all roles

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,18 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-   name: "authentication",
-   initialState: {
-      value: {
-         isAuthenticated: true,
-         roles: ["ROLE_CLERK", "ROLE_APPLICANT", "ROLE_SECRETARY"],
-      },
-   },
-   reducers: {
-      setAuthentication: (state, action) => {
-         state.value = action.payload;
-      },
-   },
+  name: "authentication",
+  initialState: {
+    value: {
+      isAuthenticated: true,
+      roles: [
+        "ROLE_CLERK",
+        "ROLE_APPLICANT",
+        "ROLE_SECRETARY",
+        "ROLE_REVIEWER",
+        "ROLE_ADMIN",
+      ],
+    },
+  },
+  reducers: {
+    setAuthentication: (state, action) => {
+      state.value = action.payload;
+    },
+  },
 });
 
 export const { setAuthentication } = authSlice.actions;
