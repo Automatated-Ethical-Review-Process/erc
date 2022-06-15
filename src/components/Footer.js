17db7ccbf30d0df2776c39db76b95f61170bf789
dilshan <nishadadilshan@gmail.com>
2022-06-15 15:15:52 +0530

    fixed unused components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,70 +6,70 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const FooterContainer = styled.footer`
-   text-align: center;
-   width: 100%;
-   background: black;
-   opacity: 0.6;
-   position: absolute;
-   /* bottom: 0px; */
-   ${(props) => props.bottom}: 0px;
+  text-align: center;
+  width: 100%;
+  background: black;
+  opacity: 0.6;
+  position: absolute;
+  /* bottom: 0px; */
+  ${(props) => props.bottom}: 0px;
 `;
 export default function Footer() {
-   const text = `© ${new Date().getFullYear()} Department of Computer Science. All Rights Reserved`;
-   const [bottom, setBottom] = useState("bottom");
-   const location = useLocation();
+  const text = `© ${new Date().getFullYear()} Department of Computer Science. All Rights Reserved`;
+  const [bottom, setBottom] = useState("bottom");
+  const location = useLocation();
 
-   const setFooter = () => {
-      let ah = window.screen.availHeight;
-      let sh = document.body.scrollHeight;
-      ah < sh
-         ? setBottom("null")
-         : ah > 530 && ah < 601
-         ? location.pathname !== "/"
-            ? setBottom("bottom")
-            : setBottom("null")
-         : setBottom("bottom");
-   };
-   window.onresize = function () {
-      setFooter();
-   };
-   document.onclick = function () {
-      setFooter();
-   };
-   useEffect(() => {
-      setFooter();
-   }, [location]);
-   return (
-      <FooterContainer bottom={bottom}>
-         <Typography
-            variant="title"
-            display="block"
-            sx={{
-               mr: 2,
-               p: 1,
-               display: { xs: "block", md: "none" },
-               fontSize: 15,
-               letterSpacing: ".1rem",
-               color: "white",
-               textDecoration: "none",
-            }}
-         >
-            {text}
-         </Typography>
-         <Typography
-            variant="title"
-            sx={{
-               mr: 2,
-               lineHeight: 3.3,
-               display: { xs: "none", md: "block" },
-               fontSize: 15,
-               letterSpacing: ".1rem",
-               color: "white",
-               textDecoration: "none",
-            }}
-         >
-            {text}
-         </Typography>
-      </FooterContainer>
-   );
+  const setFooter = () => {
+    let ah = window.screen.availHeight;
+    let sh = document.body.scrollHeight;
+    ah < sh
+      ? setBottom("null")
+      : ah > 530 && ah < 601
+      ? location.pathname !== "/"
+        ? setBottom("bottom")
+        : setBottom("null")
+      : setBottom("bottom");
+  };
+  window.onresize = function () {
+    setFooter();
+  };
+  document.onclick = function () {
+    setFooter();
+  };
+  useEffect(() => {
+    setFooter();
+  }, [location, setFooter]);
+  return (
+    <FooterContainer bottom={bottom}>
+      <Typography
+        variant="title"
+        display="block"
+        sx={{
+          mr: 2,
+          p: 1,
+          display: { xs: "block", md: "none" },
+          fontSize: 15,
+          letterSpacing: ".1rem",
+          color: "white",
+          textDecoration: "none",
+        }}
+      >
+        {text}
+      </Typography>
+      <Typography
+        variant="title"
+        sx={{
+          mr: 2,
+          lineHeight: 3.3,
+          display: { xs: "none", md: "block" },
+          fontSize: 15,
+          letterSpacing: ".1rem",
+          color: "white",
+          textDecoration: "none",
+        }}
+      >
+        {text}
+      </Typography>
+    </FooterContainer>
+  );
 }
--- src/containers/sidebar/applicant/oldSubmissions/OldSubmissions.js
+++ src/containers/sidebar/applicant/oldSubmissions/OldSubmissions.js
@@ -1,28 +1,17 @@
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
+
 import Grid from "@mui/material/Grid";
-import { Button, Container, Typography } from "@mui/material";
-import Divider from "@mui/material/Divider";
+import { Container } from "@mui/material";
 
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import { getProposal, getProposals } from "services/proposalService";
+import { getProposals } from "services/proposalService";
 
 import DataGrid from "components/common/DataGrid";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 function GetView() {
   const onclick = useNavigate();
   const location = useLocation();
-  const { pid } = useParams();
 
   const proposals = getProposals();
 
