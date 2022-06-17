df8b707249455d42e24c26119d7342ccd05cec29
dilshan <nishadadilshan@gmail.com>
2022-06-17 08:59:28 +0530

    change the correction of current-submissions path

--- src/containers/dashboard/ApplicantDashboard.js
+++ src/containers/dashboard/ApplicantDashboard.js
@@ -1,19 +1,19 @@
 import Dashboard from "containers/dashboard/Dashboard";
 
 const data = [
-   {
-      value: "New Submission",
-      weight: 12,
-      path: "/applicant/new-submission",
-   },
-   {
-      value: "Current Submission",
-      weight: 6,
-      path: "/applicant/current-submission",
-   },
-   { value: "Old Submissions", weight: 6, path: "/applicant/old-submissions" },
+  {
+    value: "New Submission",
+    weight: 12,
+    path: "/applicant/new-submission",
+  },
+  {
+    value: "Current Submission",
+    weight: 6,
+    path: "/applicant/current-submissions",
+  },
+  { value: "Old Submissions", weight: 6, path: "/applicant/old-submissions" },
 ];
 
 export default function ApplicantDashboard() {
-   return <Dashboard data={data} />;
+  return <Dashboard data={data} />;
 }
