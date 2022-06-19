6b95363f0a2e96132ea12945adf12ae20c6b4925
rking32 <sljohnwick@gmail.com>
2022-06-19 12:28:10 +0530

    add breadcrumbs

--- src/layouts/sidebar/SidebarLayout.js
+++ src/layouts/sidebar/SidebarLayout.js
@@ -1,13 +1,52 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useMemo } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import { Breadcrumbs, Chip } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 import NavigationBar from "components/NavigationBar";
 import RoleNavigationBar from "components/RoleNavigationBar";
 
 export default function SidebarLayout({ role, sideBarItems }) {
+   const { pathname } = useLocation();
+   const navigate = useNavigate();
+
+   const paths = useMemo(
+      () =>
+         pathname
+            .split("/")
+            .slice(1)
+            .reduce(
+               (data, curPath, index) => {
+                  data.push({
+                     name: curPath,
+                     link: data[index].link + "/" + curPath,
+                  });
+                  return data;
+               },
+               [{ link: "" }]
+            )
+            .slice(1),
+      [pathname]
+   );
+
+   const curPath = paths.pop();
+
    return (
       <NavigationBar title="ERC System" sideBarItems={sideBarItems}>
          <RoleNavigationBar role={role} />
+         <Breadcrumbs m={2} separator={<NavigateNextIcon fontSize="small" />}>
+            {paths.map((path, index) => (
+               <Chip
+                  key={index}
+                  label={path.name}
+                  onClick={() => navigate(path.link)}
+                  icon={index === 0 ? <HomeIcon fontSize="small" /> : null}
+               />
+            ))}
+            <Chip color="primary" label={curPath.name} />
+         </Breadcrumbs>
          <Outlet />
       </NavigationBar>
    );
