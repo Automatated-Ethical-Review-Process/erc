ee51f5938b66ffe3b22b52c9852d0d4cb4c5ad73
malindu madhusankha <dpmmadhusankha@gmail.com>
2022-07-14 21:18:49 +0530

    Reviewer reqests view of the secretary

--- src/layouts/sidebar/SecretaryLayout.js
+++ src/layouts/sidebar/SecretaryLayout.js
@@ -3,38 +3,44 @@ import PageviewIcon from "@mui/icons-material/Pageview";
 import AssignmentLateIcon from "@mui/icons-material/AssignmentLate";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ThreePIcon from "@mui/icons-material/ThreeP";
 
 import SidebarLayout from "./SidebarLayout";
 import Roles from "config/roles";
 
 const sideBarItems = [
-   {
-      path: "/secretary/unassigned",
-      icon: <AssignmentLateIcon />,
-      text: "Unassigned",
-   },
-   {
-      path: "/secretary/in-review",
-      icon: <PageviewIcon />,
-      text: "In Review",
-   },
-   {
-      path: "/secretary/reviewed",
-      icon: <GradingIcon />,
-      text: "Reviewed",
-   },
-   {
-      path: "/secretary/archived",
-      icon: <ArchiveIcon />,
-      text: "Archived",
-   },
-   {
-      path: "/secretary/user-management",
-      icon: <AccountBoxIcon />,
-      text: "User Management",
-   },
+  {
+    path: "/secretary/unassigned",
+    icon: <AssignmentLateIcon />,
+    text: "Unassigned",
+  },
+  {
+    path: "/secretary/in-review",
+    icon: <PageviewIcon />,
+    text: "In Review",
+  },
+  {
+    path: "/secretary/reviewed",
+    icon: <GradingIcon />,
+    text: "Reviewed",
+  },
+  {
+    path: "/secretary/archived",
+    icon: <ArchiveIcon />,
+    text: "Archived",
+  },
+  {
+    path: "/secretary/user-management",
+    icon: <AccountBoxIcon />,
+    text: "User Management",
+  },
+  {
+    path: "/secretary/reviewer-requests",
+    icon: <ThreePIcon />,
+    text: "Reviewer requests",
+  },
 ];
 
 export default function SecretaryLayout() {
-   return <SidebarLayout role={Roles.secretary} sideBarItems={sideBarItems} />;
+  return <SidebarLayout role={Roles.secretary} sideBarItems={sideBarItems} />;
 }
