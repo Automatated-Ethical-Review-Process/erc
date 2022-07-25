4acef7f49bc6bbaf4c204ab1c9332ed6db974596
rking32 <sljohnwick@gmail.com>
2022-07-25 21:30:01 +0530

    fix breadcrumb + show profile alignments

--- src/api/auth/api.js
+++ src/api/auth/api.js
@@ -6,252 +6,252 @@ import authService from "services/auth";
 import Roles from "config/roles";
 
 const authApi = createApi({
-   reducerPath: "api/auth",
-   tagTypes: ["authUser"],
-   baseQuery: authQuery,
-   endpoints: (build) => ({
-      getUser: build.query({
-         queryFn(_, _api, __, _baseQuery) {
-            const { auth } = _api.getState();
-            if (auth.isAuthenticated) {
-               return _baseQuery("/current-user");
-            }
-            return { error: {} };
-         },
-         providesTags: ["authUser"],
+  reducerPath: "api/auth",
+  tagTypes: ["authUser"],
+  baseQuery: authQuery,
+  endpoints: (build) => ({
+    getUser: build.query({
+      queryFn(_, _api, __, _baseQuery) {
+        const { auth } = _api.getState();
+        if (auth.isAuthenticated) {
+          return _baseQuery("/current-user");
+        }
+        return { error: {} };
+      },
+      providesTags: ["authUser"],
+    }),
+    signupVerify: build.mutation({
+      query: (body) => ({
+        url: "/create-user/token",
+        method: "POST",
+        body,
       }),
-      signupVerify: build.mutation({
-         query: (body) => ({
-            url: "/create-user/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    signup: build.mutation({
+      query: ({ id, body }) => ({
+        url: "/create-user",
+        method: "POST",
+        params: { id },
+        body,
       }),
-      signup: build.mutation({
-         query: ({ id, body }) => ({
-            url: "/create-user",
-            method: "POST",
-            params: { id },
-            body,
-         }),
-         invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+      invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+    }),
+    login: build.mutation({
+      query: (body) => ({
+        url: "/token/generate",
+        method: "POST",
+        body,
       }),
-      login: build.mutation({
-         query: (body) => ({
-            url: "/token/generate",
-            method: "POST",
-            body,
-         }),
-         invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+      invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+    }),
+    logout: build.mutation({
+      query: () => ({
+        url: "/logout",
+        method: "POST",
       }),
-      logout: build.mutation({
-         query: () => ({
-            url: "/logout",
-            method: "POST",
-         }),
-         invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+      invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
+    }),
+    validate: build.mutation({
+      query: (id) => ({
+        url: "/request/validate",
+        method: "POST",
+        params: { id },
       }),
-      validate: build.mutation({
-         query: (id) => ({
-            url: "/request/validate",
-            method: "POST",
-            params: { id },
-         }),
+    }),
+    updateEmailVerify: build.mutation({
+      query: (body) => ({
+        url: "/update/email/send/token",
+        method: "POST",
+        body,
       }),
-      updateEmailVerify: build.mutation({
-         query: (body) => ({
-            url: "/update/email/send/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    updateEmail: build.mutation({
+      query: (id) => ({
+        url: "/update/email",
+        method: "PUT",
+        params: { id },
+        invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
       }),
-      updateEmail: build.mutation({
-         query: (id) => ({
-            url: "/update/email",
-            method: "PUT",
-            params: { id },
-            invalidatesTags: (_, e) => (e ? [] : ["authUser"]),
-         }),
+    }),
+    forgotPasswordVerify: build.mutation({
+      query: (body) => ({
+        url: "/update/password/forgot/token",
+        method: "POST",
+        body,
       }),
-      forgotPasswordVerify: build.mutation({
-         query: (body) => ({
-            url: "/update/password/forgot/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    forgotPassword: build.mutation({
+      query: ({ id, password }) => ({
+        url: "/update/password/forgot",
+        method: "POST",
+        params: { id },
+        body: { password },
       }),
-      forgotPassword: build.mutation({
-         query: ({ id, password }) => ({
-            url: "/update/password/forgot",
-            method: "POST",
-            params: { id },
-            body: { password },
-         }),
+    }),
+    checkPassword: build.mutation({
+      query: (body) => ({
+        url: "/check/password",
+        method: "POST",
+        body,
       }),
-      checkPassword: build.mutation({
-         query: (body) => ({
-            url: "/check/password",
-            method: "POST",
-            body,
-         }),
+    }),
+    updatePassword: build.mutation({
+      query: (body) => ({
+        url: "/update/password",
+        method: "PUT",
+        body,
       }),
-      updatePassword: build.mutation({
-         query: (body) => ({
-            url: "/update/password",
-            method: "PUT",
-            body,
-         }),
+    }),
+    updateRoles: build.mutation({
+      query: (body) => ({
+        url: "/update/roles",
+        method: "PUT",
+        body,
       }),
-      updateRoles: build.mutation({
-         query: (body) => ({
-            url: "/update/roles",
-            method: "PUT",
-            body,
-         }),
+    }),
+    inviteReviewer: build.mutation({
+      query: (body) => ({
+        url: "/create-user/invite/reviewer/token",
+        method: "POST",
+        body,
       }),
-      inviteReviewer: build.mutation({
-         query: (body) => ({
-            url: "/create-user/invite/reviewer/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    inviteClerk: build.mutation({
+      query: (body) => ({
+        url: "/create-user/invite/clerk/token",
+        method: "POST",
+        body,
       }),
-      inviteClerk: build.mutation({
-         query: (body) => ({
-            url: "/create-user/invite/clerk/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    inviteSecretary: build.mutation({
+      query: (body) => ({
+        url: "/create-user/invite/secretary/token",
+        method: "POST",
+        body,
       }),
-      inviteSecretary: build.mutation({
-         query: (body) => ({
-            url: "/create-user/invite/secretary/token",
-            method: "POST",
-            body,
-         }),
+    }),
+    toggleEnabled: build.mutation({
+      query: () => ({
+        url: "/user/enable",
+        method: "PUT",
       }),
-      toggleEnabled: build.mutation({
-         query: () => ({
-            url: "/user/enable",
-            method: "PUT",
-         }),
+    }),
+    toggleUserEnabled: build.mutation({
+      query: (id) => ({
+        url: `/user/enable/${id}`,
+        method: "PUT",
       }),
-      toggleUserEnabled: build.mutation({
-         query: (id) => ({
-            url: `/user/enable/${id}`,
-            method: "PUT",
-         }),
+    }),
+    toggleUserLocked: build.mutation({
+      query: (id) => ({
+        url: `/user/lock/${id}`,
+        method: "PUT",
       }),
-      toggleUserLocked: build.mutation({
-         query: (id) => ({
-            url: `/user/lock/${id}`,
-            method: "PUT",
-         }),
+    }),
+    setUserVerified: build.mutation({
+      query: (id) => ({
+        url: `/user/verified/${id}`,
+        method: "PUT",
       }),
-      toggleUserVerified: build.mutation({
-         query: (id) => ({
-            url: `/user/verified/${id}`,
-            method: "PUT",
-         }),
-      }),
-   }),
+    }),
+  }),
 });
 
 export const {
-   useLazyGetUserQuery, //
-   useSignupVerifyMutation, //
-   useSignupMutation, //
-   useLoginMutation, //
-   useLogoutMutation, //
-   useValidateMutation, //
-   useUpdateEmailVerifyMutation, //
-   useUpdateEmailMutation, //
-   useForgotPasswordVerifyMutation, //
-   useForgotPasswordMutation, //
-   useCheckPasswordMutation, //
-   useUpdatePasswordMutation, //
-   useUpdateRolesMutation,
-   useInviteReviewerMutation,
-   useInviteClerkMutation,
-   useInviteSecretaryMutation,
-   useToggleEnabledMutation,
-   useToggleUserEnabledMutation,
-   useToggleUserLockedMutation,
-   useToggleUserVerifiedMutation,
+  useLazyGetUserQuery, //
+  useSignupVerifyMutation, //
+  useSignupMutation, //
+  useLoginMutation, //
+  useLogoutMutation, //
+  useValidateMutation, //
+  useUpdateEmailVerifyMutation, //
+  useUpdateEmailMutation, //
+  useForgotPasswordVerifyMutation, //
+  useForgotPasswordMutation, //
+  useCheckPasswordMutation, //
+  useUpdatePasswordMutation, //
+  useUpdateRolesMutation,
+  useInviteReviewerMutation,
+  useInviteClerkMutation,
+  useInviteSecretaryMutation,
+  useToggleEnabledMutation,
+  useToggleUserEnabledMutation,
+  useToggleUserLockedMutation,
+  useSetUserVerifiedMutation,
 } = authApi;
 
 export default authApi;
 
 const initialUser = {
-   id: "",
-   email: "",
-   roles: [],
+  id: "",
+  email: "",
+  roles: [],
 };
 
 const fixRoles = (roles) => {
-   if (roles.includes(Roles.e_reviewer) || roles.includes(Roles.i_reviewer)) {
-      return [...roles, Roles.reviewer];
-   }
-   return roles;
+  if (roles.includes(Roles.e_reviewer) || roles.includes(Roles.i_reviewer)) {
+    return [...roles, Roles.reviewer];
+  }
+  return roles;
 };
 
 const reset = (auth) => {
-   auth.user = initialUser;
-   auth.isAuthenticated = false;
-   authService.reset();
+  auth.user = initialUser;
+  auth.isAuthenticated = false;
+  authService.reset();
 };
 
 const authSlice = createSlice({
-   name: "auth",
-   initialState: {
-      user: initialUser,
-      isAuthenticated: authService.hasAccess,
-   },
-   reducers: {
-      refreshFulfilled(auth, { payload }) {
-         auth.isAuthenticated = true;
-         authService.access = payload.access;
-      },
-      refreshRejected: reset,
-   },
-   extraReducers: (builder) => {
-      builder.addMatcher(
-         authApi.endpoints.getUser.matchFulfilled,
-         (auth, { payload }) => {
-            auth.user = {
-               id: payload.id,
-               email: payload.email,
-               roles: fixRoles(payload.roles),
-            };
-         }
-      );
-      builder.addMatcher(
-         authApi.endpoints.getUser.matchRejected,
-         (auth, { payload }) => (payload?.status ? reset(auth) : void 0)
-      );
+  name: "auth",
+  initialState: {
+    user: initialUser,
+    isAuthenticated: authService.hasAccess,
+  },
+  reducers: {
+    refreshFulfilled(auth, { payload }) {
+      auth.isAuthenticated = true;
+      authService.access = payload.access;
+    },
+    refreshRejected: reset,
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      authApi.endpoints.getUser.matchFulfilled,
+      (auth, { payload }) => {
+        auth.user = {
+          id: payload.id,
+          email: payload.email,
+          roles: fixRoles(payload.roles),
+        };
+      }
+    );
+    builder.addMatcher(
+      authApi.endpoints.getUser.matchRejected,
+      (auth, { payload }) => (payload?.status ? reset(auth) : void 0)
+    );
 
-      builder.addMatcher(
-         authApi.endpoints.login.matchFulfilled,
-         (auth, { payload: { roles, access, refresh } }) => {
-            auth.user.roles = fixRoles(roles);
-            auth.isAuthenticated = true;
-            authService.update({ access, refresh });
-         }
-      );
+    builder.addMatcher(
+      authApi.endpoints.login.matchFulfilled,
+      (auth, { payload: { roles, access, refresh } }) => {
+        auth.user.roles = fixRoles(roles);
+        auth.isAuthenticated = true;
+        authService.update({ access, refresh });
+      }
+    );
 
-      builder.addMatcher(
-         isAnyOf(
-            authApi.endpoints.logout.matchFulfilled,
-            authApi.endpoints.logout.matchRejected
-         ),
-         reset
-      );
-   },
+    builder.addMatcher(
+      isAnyOf(
+        authApi.endpoints.logout.matchFulfilled,
+        authApi.endpoints.logout.matchRejected
+      ),
+      reset
+    );
+  },
 });
 
 initRefreshActions(
-   authSlice.actions.refreshFulfilled,
-   authSlice.actions.refreshRejected
+  authSlice.actions.refreshFulfilled,
+  authSlice.actions.refreshRejected
 );
 
 export const { reducer } = authSlice;
--- src/components/profile/ShowProfile.js
+++ src/components/profile/ShowProfile.js
@@ -1,5 +1,4 @@
 import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -18,131 +17,111 @@ import { useGetMeQuery } from "api/data/user";
 import LoadingCircle from "components/common/LoadingCircle";
 
 function ImageAvatar() {
-   return (
-      <Avatar
-         alt="Profile Image"
-         src={Image}
-         sx={{ width: 200, height: 200 }}
-      />
-   );
+  return (
+    <Avatar alt="Profile Image" src={Image} sx={{ width: 200, height: 200 }} />
+  );
 }
-
-const Item = styled(Paper)(({ theme }) => ({
-   padding: theme.spacing(1),
-   textAlign: "center",
-   minHeight: theme.spacing(5),
-}));
 
 function GridTitle({ title }) {
-   return (
-      <Grid item xs={6} justifyContent="center">
-         <Typography variant="h7">{title}</Typography>
-      </Grid>
-   );
+  return (
+    <Grid item xs={6} justifyContent="center">
+      <Typography variant="h7">{title}</Typography>
+    </Grid>
+  );
 }
 
-function GridItem({ title, body }) {
-   return (
-      <>
-         <GridTitle title={title} />
-         <Grid item xs={6}>
-            <Item>
-               <Typography
-                  sx={{ wordWrap: "break-word", whiteSpace: "pre-line" }}
-               >
-                  {body}
-               </Typography>
-            </Item>
-         </Grid>
-      </>
-   );
+function GridItem({ title, body, textAlign = "center" }) {
+  return (
+    <>
+      <GridTitle title={title} />
+      <Grid item xs={6}>
+        <Paper sx={{ p: 1, minHeight: 5, textAlign }}>
+          <Typography sx={{ wordWrap: "break-word", whiteSpace: "pre-line" }}>
+            {body}
+          </Typography>
+        </Paper>
+      </Grid>
+    </>
+  );
 }
 
 export function Content() {
-   const navigate = useNavigate();
-   const { user } = useAuth();
-   const { data = {}, isLoading } = useGetMeQuery();
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const { data = {}, isLoading } = useGetMeQuery();
 
-   return (
-      <Container maxWidth={"md"} sx={{ pb: 10 }}>
-         <LoadingCircle isLoading={isLoading} />
-         <Grid
-            container
-            direction="column"
-            alignItems="center"
-            marginTop={2}
-            marginBottom={2}
-         >
-            <ImageAvatar />
-         </Grid>
-         <Box sx={{ width: "100%" }}>
-            <Grid container rowSpacing={2}>
-               <GridItem title="Name" body={data.name} />
-               <GridItem title="Email" body={user.email} />
-               <GridItem title="Phone Number" body={data.mobileNumber} />
-               <GridItem title="Land Number" body={data.landNumber} />
-               <GridItem
-                  title="NIC / Passport"
-                  body={data.nic || data.passport}
-               />
-               <GridItem title="Address" body={data.address} />
-               <GridItem
-                  title="Educational Qualifications"
-                  body={(data.educationalQualifications || []).join("\n")}
-               />
-               {data.isUnderGraduate && (
-                  <>
-                     <GridTitle title="Is Undergraduate" />
-                     <Grid item xs={6}>
-                        <Checkbox checked={data.isUnderGraduate} />
-                     </Grid>
-                     {data.isUnderGraduate ? (
-                        <>
-                           <GridItem
-                              title="University"
-                              body={data.university}
-                           />
-                           <GridItem title="Faculty" body={data.faculty} />
-                           <GridItem
-                              title="Registration Number"
-                              body={data.registrationNumber}
-                           />
-                           <GridItem title="Year" body={data.year} />
-                        </>
-                     ) : (
-                        <>
-                           <GridItem
-                              title="Occupation"
-                              body={data.occupation}
-                           />
-                           <GridItem title="Position" body={data.position} />
-                        </>
-                     )}
-                  </>
-               )}
-            </Grid>
-         </Box>
-         <Fab
-            variant="extended"
-            color="secondary"
-            sx={(t) => ({
-               position: "fixed",
-               right: t.spacing(4),
-               bottom: t.spacing(4),
-            })}
-            onClick={() => navigate("/profile/edit")}
-         >
-            <EditIcon sx={{ mr: 1 }} />
-            Edit
-         </Fab>
-      </Container>
-   );
+  return (
+    <Container maxWidth={"md"} sx={{ pb: 10 }}>
+      <LoadingCircle isLoading={isLoading} />
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        marginTop={2}
+        marginBottom={2}
+      >
+        <ImageAvatar />
+      </Grid>
+      <Box sx={{ width: "100%" }}>
+        <Grid container rowSpacing={2}>
+          <GridItem title="Name" body={data.name} />
+          <GridItem title="Email" body={user.email} />
+          <GridItem title="Phone Number" body={data.mobileNumber} />
+          <GridItem title="Land Number" body={data.landNumber} />
+          <GridItem title="NIC / Passport" body={data.nic || data.passport} />
+          <GridItem title="Address" body={data.address} />
+          <GridItem
+            title="Educational Qualifications"
+            body={(data.educationalQualifications || []).join("\n")}
+            textAlign="left"
+          />
+          {data.isUnderGraduate && (
+            <>
+              <GridTitle title="Is Undergraduate" />
+              <Grid item xs={6}>
+                <Checkbox checked={data.isUnderGraduate} />
+              </Grid>
+              {data.isUnderGraduate ? (
+                <>
+                  <GridItem title="University" body={data.university} />
+                  <GridItem title="Faculty" body={data.faculty} />
+                  <GridItem
+                    title="Registration Number"
+                    body={data.registrationNumber}
+                  />
+                  <GridItem title="Year" body={data.year} />
+                </>
+              ) : (
+                <>
+                  <GridItem title="Occupation" body={data.occupation} />
+                  <GridItem title="Position" body={data.position} />
+                </>
+              )}
+            </>
+          )}
+        </Grid>
+      </Box>
+      <Fab
+        variant="extended"
+        color="secondary"
+        sx={(t) => ({
+          position: "fixed",
+          right: t.spacing(4),
+          bottom: t.spacing(4),
+        })}
+        onClick={() => navigate("/profile/edit")}
+      >
+        <EditIcon sx={{ mr: 1 }} />
+        Edit
+      </Fab>
+    </Container>
+  );
 }
 
 export default function ShowProfile() {
-   return (
-      <NavigationBar title="Profile">
-         <Content />
-      </NavigationBar>
-   );
+  return (
+    <NavigationBar title="Profile">
+      <Content />
+    </NavigationBar>
+  );
 }
