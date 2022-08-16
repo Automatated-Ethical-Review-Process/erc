2f5f62e90e8f8047d6696bebd88e94c13d88eb60
dilshan <nishadadilshan@gmail.com>
2022-08-16 16:50:29 +0530

    Edited system theme color(dark mood light mood)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState, useCallback, useMemo } from "react";
 
-import { useMediaQuery } from "@mui/material";
+// import { useMediaQuery } from "@mui/material";
 import { ThemeProvider } from "styled-components";
 import { SnackbarProvider } from "notistack";
 import {
-   createTheme,
-   ThemeProvider as MuiThemeProvider,
+  createTheme,
+  ThemeProvider as MuiThemeProvider,
 } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -15,36 +15,40 @@ import AppRoutes from "routes";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function App() {
-   const preferDark = useMediaQuery("(prefers-color-scheme: dark)");
-   const [mode, setMode] = useState(preferDark ? "dark" : "light");
+  // const preferDark = useMediaQuery("(prefers-color-scheme: dark)");
+  const [mode, setMode] = useState(localStorage.getItem("theme") || "light");
 
-   const toggleTheme = useCallback(() => {
-      setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-   }, [setMode]);
+  const toggleTheme = useCallback(() => {
+    setMode((prevMode) => {
+      const pageTheme = prevMode === "light" ? "dark" : "light";
+      localStorage.setItem("theme", pageTheme);
+      return pageTheme;
+    });
+  }, [setMode]);
 
-   const theme = useMemo(
-      () =>
-         createTheme({
-            palette: {
-               mode,
-               ...getPalette(mode === "light"),
-            },
-            isLight: mode === "light",
-            toggleTheme,
-         }),
-      [mode, toggleTheme]
-   );
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          ...getPalette(mode === "light"),
+        },
+        isLight: mode === "light",
+        toggleTheme,
+      }),
+    [mode, toggleTheme]
+  );
 
-   return (
-      <MuiThemeProvider theme={theme}>
-         <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <SnackbarProvider maxSnack={5} preventDuplicate={true}>
-               <AppRoutes />
-            </SnackbarProvider>
-         </ThemeProvider>
-      </MuiThemeProvider>
-   );
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider maxSnack={5} preventDuplicate={true}>
+          <AppRoutes />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
