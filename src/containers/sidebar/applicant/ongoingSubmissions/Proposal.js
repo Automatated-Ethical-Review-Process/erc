1c68ba52b8917a96f8f92f351276491b0fbb114f
rking32 <sljohnwick@gmail.com>
2022-09-02 13:51:48 +0530

    submit new version only if possible

--- src/containers/sidebar/applicant/ongoingSubmissions/Proposal.js
+++ src/containers/sidebar/applicant/ongoingSubmissions/Proposal.js
@@ -1,15 +1,28 @@
+import { useGetLatestVersionQuery } from "api/data/version";
 import BaseProposal from "components/proposals/Proposal";
-import { useNavigate } from "react-router-dom";
+import { VersionStatus } from "config/enums";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Proposal() {
   const navigate = useNavigate();
+
+  const { pid } = useParams();
+  const { data: { status } = {}, isLoading } = useGetLatestVersionQuery(pid);
+
+  let rightButton = null;
+
+  if (status === VersionStatus.minor || status === VersionStatus.major) {
+    rightButton = {
+      text: "Submit a new version",
+      onClick: () => navigate("new-version"),
+    };
+  }
+
   return (
     <BaseProposal
+      loading={isLoading}
       extraFields={{ status: "Status" }}
-      rightButton={{
-        text: "Submit New Version",
-        onClick: () => navigate("new-version"),
-      }}
+      rightButton={rightButton}
     />
   );
 }
