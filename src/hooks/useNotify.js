55903bd8b4f111dea21d969fdaf0da7fae09d1bc
dilshan <nishadadilshan@gmail.com>
2022-09-03 08:28:08 +0530

    optimized useNotify in hooks useNotify

--- src/hooks/useNotify.js
+++ src/hooks/useNotify.js
@@ -1,31 +1,29 @@
 import { Button } from "@mui/material";
 import { useSnackbar } from "notistack";
-import { useNavigate } from "react-router-dom";
 const useNotify = (persist = false) => {
-   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-   const navigate = useNavigate();
-   const notify = (text, variant = "default", options = {}) =>
-      enqueueSnackbar(text, {
-         variant,
-         persist,
-         autoHideDuration: 5000,
-         action: (id) =>
-            (options.persist || (options.persist === undefined && persist)) && (
-               <Button
-                  variant="text"
-                  color="inherit"
-                  onClick={() => {
-                     closeSnackbar(id);
-                     options.onClick ? options.onClick() : void 0;
-                  }}
-               >
-                  {options.label || "Dismiss"}
-               </Button>
-            ),
-         ...options,
-      });
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const notify = (text, variant = "default", options = {}) =>
+    enqueueSnackbar(text, {
+      variant,
+      persist,
+      autoHideDuration: 5000,
+      action: (id) =>
+        (options.persist || (options.persist === undefined && persist)) && (
+          <Button
+            variant="text"
+            color="inherit"
+            onClick={() => {
+              closeSnackbar(id);
+              options.onClick ? options.onClick() : void 0;
+            }}
+          >
+            {options.label || "Dismiss"}
+          </Button>
+        ),
+      ...options,
+    });
 
-   return { notify, closeNotify: closeSnackbar };
+  return { notify, closeNotify: closeSnackbar };
 };
 
 export default useNotify;
