4e6f7267edc5bc080c8314b6860f8e660bb977bf
rking32 <sljohnwick@gmail.com>
2022-09-06 02:36:06 +0530

    fix dashboard layouts

--- src/containers/dashboard/ApplicantDashboard.js
+++ src/containers/dashboard/ApplicantDashboard.js
@@ -1,6 +1,8 @@
+import Roles from "config/roles";
 import Dashboard from "containers/dashboard/Dashboard";
+import useRoles from "hooks/useRoles";
 
-const data = [
+const dataRest = [
   {
     value: "New Submission",
     weight: 12,
@@ -16,9 +18,35 @@ const data = [
     weight: 6,
     path: "/applicant/ongoing-submissions",
   },
-  { value: "Old Submissions", weight: 12, path: "/applicant/old-submissions" },
 ];
 
+const other = {
+  value: "Old Submissions",
+  weight: 12,
+  path: "/applicant/old-submissions",
+};
+
+const reviewer = {
+  value: "Switch to Reviewer",
+  weight: 6,
+  path: "/reviewer",
+};
+
 export default function ApplicantDashboard() {
+  const roles = useRoles();
+
+  const data = [...dataRest];
+
+  data.push(other);
+
+  if (roles.includes(Roles.reviewer)) {
+    data.push(reviewer);
+  }
+
+  if (data.length === 4) {
+    other.weight = 12;
+  } else {
+    other.weight = 6;
+  }
   return <Dashboard data={data} />;
 }
--- src/containers/dashboard/ReviewerDashboard.js
+++ src/containers/dashboard/ReviewerDashboard.js
@@ -3,48 +3,58 @@ import Roles from "config/roles";
 import useRoles from "hooks/useRoles";
 
 const dataRest = [
-   { value: "Pending Proposals", weight: 6, path: "/reviewer/pending" },
-   {
-      value: "Reviewing Proposals",
-      weight: 6,
-      path: "/reviewer/reviewing",
-   },
-   {
-      value: "Reviewed Proposals",
-      weight: 6,
-      path: "/reviewer/reviewed",
-   },
+  { value: "Pending Proposals", weight: 6, path: "/reviewer/pending" },
+  {
+    value: "Reviewing Proposals",
+    weight: 6,
+    path: "/reviewer/reviewing",
+  },
+  {
+    value: "Reviewed Proposals",
+    weight: 6,
+    path: "/reviewer/reviewed",
+  },
 ];
 
 const dataIReviewer = {
-   value: "Other Proposals",
-   weight: 6,
-   path: "/reviewer/other",
+  value: "Other Proposals",
+  weight: 6,
+  path: "/reviewer/other",
 };
 
 const dataApplicant = {
-   value: "Switch to Applicant",
-   weight: 6,
-   path: "/applicant",
+  value: "Switch to Applicant",
+  weight: 6,
+  path: "/applicant",
+};
+
+const dataSecretary = {
+  value: "Switch to Secretary",
+  weight: 12,
+  path: "/secretary",
 };
 
 export default function ReviewerDashboard() {
-   const roles = useRoles();
+  const roles = useRoles();
 
-   const data = [...dataRest];
+  const data = [...dataRest];
 
-   if (roles.includes(Roles.i_reviewer)) {
-      data.push(dataIReviewer);
-   }
-   if (roles.includes(Roles.applicant)) {
-      data.push(dataApplicant);
-   }
+  if (roles.includes(Roles.i_reviewer)) {
+    data.push(dataIReviewer);
+  }
+  if (roles.includes(Roles.applicant)) {
+    data.push(dataApplicant);
+  }
 
-   if (data.length === 5) {
-      dataApplicant.weight = 12;
-   } else {
-      dataApplicant.weight = 6;
-   }
+  if (data.length === 5) {
+    dataApplicant.weight = 12;
+  } else {
+    dataApplicant.weight = 6;
+  }
 
-   return <Dashboard data={data} />;
+  if (roles.includes(Roles.secretary)) {
+    data.push(dataSecretary);
+  }
+
+  return <Dashboard data={data} />;
 }
